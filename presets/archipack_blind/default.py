d140667f67c8905dfe08f28569b0e7d8e943b757
s-leger <stephen@3dservices.ch>
2018-01-09 16:15:19 +0100

    Blind default preset

---
+++ presets/archipack_blind/default.py
@@ -0,0 +1,13 @@
+import bpy
+d = bpy.context.active_object.data.archipack_blind[0]
+bpy.ops.archipack.material(category='blind', material='DEFAULT')
+
+d.altitude = 1.7000000476837158
+d.angle = 0.0
+d.depth = 0.03999999910593033
+d.frame_depth = 0.03999999910593033
+d.frame_height = 0.20000000298023224
+d.height = 1.7000000476837158
+d.ratio = 100.0
+d.stack_inside_frame = True
+d.width = 1.0
